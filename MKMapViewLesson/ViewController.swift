b2a34c00c8a9366cf9495a7cab76730eb6fc30b7
HiroUrata <1712026@i.softbank.jp>
2021-08-28 19:18:02 +0900

    MKMapView 追加

--- MKMapViewLesson/ViewController.swift
+++ MKMapViewLesson/ViewController.swift
@@ -6,13 +6,21 @@
 //
 
 import UIKit
+import MapKit  //必須
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        //MKMapViewプロパティ
+        
+        
     }
+    
+    
 
 
 }
